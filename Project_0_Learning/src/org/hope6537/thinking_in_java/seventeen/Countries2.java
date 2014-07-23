f562964f84ea669c55217c97a796dc078f6412c0
Zhao Peng <hope6537@qq.com>
2014-07-23 13:21:45 +0800

    2014年7月23日13:21:42

--- Project_0_Learning/src/org/hope6537/thinking_in_java/seventeen/Countries2.java
+++ Project_0_Learning/src/org/hope6537/thinking_in_java/seventeen/Countries2.java
@@ -1,163 +1,8 @@
 package org.hope6537.thinking_in_java.seventeen;
 
-import java.util.AbstractMap;
-import java.util.AbstractSet;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 
-/**
- * @describe 享元设计模式
- * @author Hope6537(赵鹏)
- * @signdate 2014-7-22下午04:21:32
- * @version 0.9
- * @company Changchun University&SHXT
- */
-public class Countries {
-
-	private static class FlyWeightMap extends AbstractMap<String, String> {
-
-		private static class Entry implements Map.Entry<String, String> {
-			int index;
-
-			public Entry(int index) {
-				super();
-				this.index = index;
-			}
-
-			@Override
-			public boolean equals(Object obj) {
-				return DATA[index][0].equals(obj);
-			}
-
-			@Override
-			public String getKey() {
-				return DATA[index][0];
-			}
-
-			@Override
-			public String getValue() {
-				return DATA[index][1];
-			}
-
-			@Override
-			public String setValue(String value) {
-				System.out.println("No Setting");
-				return null;
-			}
-
-			@Override
-			public int hashCode() {
-				return DATA[index][0].hashCode();
-			}
-		}
-
-		static class EntrySet extends AbstractSet<Map.Entry<String, String>> {
-			private int size;
-
-			public EntrySet(int size) {
-				if (size < 0) {
-					this.size = 0;
-				} else if (size > DATA.length) {
-					this.size = DATA.length;
-				} else {
-					this.size = size;
-				}
-			}
-
-			@Override
-			public int size() {
-				return size;
-			}
-
-			private class Iter implements Iterator<Map.Entry<String, String>> {
-				private Entry entry = new Entry(-1);
-
-				@Override
-				public boolean hasNext() {
-					return entry.index < size - 1;
-				}
-
-				@Override
-				public Map.Entry<String, String> next() {
-					entry.index++;
-					return entry;
-				}
-
-				@Override
-				public void remove() {
-					System.out.println("No Remove");
-				}
-			}
-
-			@Override
-			public Iterator<java.util.Map.Entry<String, String>> iterator() {
-				return new Iter();
-			}
-
-		}
-
-		private static Set<Map.Entry<String, String>> entries = new EntrySet(
-				DATA.length);
-
-		public Set<Map.Entry<String, String>> entrySet() {
-			return entries;
-		}
-
-		static Map<String, String> select(final int size) {
-			return new FlyWeightMap() {
-				public Set<Map.Entry<String, String>> entrySet() {
-					return new EntrySet(size);
-				}
-			};
-		}
-
-		static Map<String, String> map = new FlyWeightMap();
-
-		public static Map<String, String> capitals() {
-			return map;
-		}
-
-		public static Map<String, String> capitals(int size) {
-			return select(size);
-		}
-
-		static List<String> names = new ArrayList<String>(map.keySet());
-
-		public static List<String> names() {
-			return names;
-		}
-
-		public static List<String> names(int size) {
-			return new ArrayList<String>(select(size).keySet());
-		}
-
-		public static void main(String[] args) {
-			print(capitals(10));
-			print(names(10));
-			print(new HashMap<String, String>(capitals(3)));
-			print(new LinkedHashMap<String, String>(capitals(3)));
-			print(new TreeMap<String, String>(capitals(3)));
-			print(new Hashtable<String, String>(capitals(3)));
-			print(new HashSet<String>(names(6)));
-			print(new LinkedHashSet<String>(names(6)));
-			print(new TreeSet<String>(names(6)));
-			print(new ArrayList<String>(names(6)));
-			print(new LinkedList<String>(names(6)));
-			print(capitals().get("BRAZIL"));
-		}
-	}
-
+public class Countries2 {
 	public static final String[][] DATA = {
 			// Africa
 			{ "ALGERIA", "Algiers" },
@@ -351,8 +196,141 @@ public class Countries {
 			{ "TRINIDAD AND TOBAGO", "Port of Spain" },
 			{ "URUGUAY", "Montevideo" }, { "VENEZUELA", "Caracas" }, };
 
-	public static void print(Object o) {
-		System.out.println(o);
+	// Use AbstractMap by implementing entrySet()
+	private static class FlyweightMap extends AbstractMap<String, String> {
+		private static class Entry implements Map.Entry<String, String> {
+			int index;
+
+			Entry(int index) {
+				this.index = index;
+			}
+
+			public boolean equals(Object o) {
+				return DATA[index][0].equals(o);
+			}
+
+			public String getKey() {
+				return DATA[index][0];
+			}
+
+			public String getValue() {
+				return DATA[index][1];
+			}
+
+			public String setValue(String value) {
+				throw new UnsupportedOperationException();
+			}
+
+			public int hashCode() {
+				return DATA[index][0].hashCode();
+			}
+		}
+
+		// Use AbstractSet by implementing size() & iterator()
+		static class EntrySet extends AbstractSet<Map.Entry<String, String>> {
+			private int size;
+
+			EntrySet(int size) {
+				if (size < 0)
+					this.size = 0;
+				// Can't be any bigger than the array:
+				else if (size > DATA.length)
+					this.size = DATA.length;
+				else
+					this.size = size;
+			}
+
+			public int size() {
+				return size;
+			}
+
+			private class Iter implements Iterator<Map.Entry<String, String>> {
+				// Only one Entry object per Iterator:
+				private Entry entry = new Entry(-1);
+
+				public boolean hasNext() {
+					return entry.index < size - 1;
+				}
+
+				public Map.Entry<String, String> next() {
+					entry.index++;
+					return entry;
+				}
+
+				public void remove() {
+					throw new UnsupportedOperationException();
+				}
+			}
+
+			public Iterator<Map.Entry<String, String>> iterator() {
+				return new Iter();
+			}
+		}
+
+		private static Set<Map.Entry<String, String>> entries = new EntrySet(
+				DATA.length);
+
+		public Set<Map.Entry<String, String>> entrySet() {
+			return entries;
+		}
 	}
 
-}
+	// Create a partial map of 'size' countries:
+	static Map<String, String> select(final int size) {
+		return new FlyweightMap() {
+			public Set<Map.Entry<String, String>> entrySet() {
+				return new EntrySet(size);
+			}
+		};
+	}
+
+	static Map<String, String> map = new FlyweightMap();
+
+	public static Map<String, String> capitals() {
+		return map; // The entire map
+	}
+
+	public static Map<String, String> capitals(int size) {
+		return select(size); // A partial map
+	}
+
+	static List<String> names = new ArrayList<String>(map.keySet());
+
+	// All the names:
+	public static List<String> names() {
+		return names;
+	}
+
+	// A partial list:
+	public static List<String> names(int size) {
+		return new ArrayList<String>(select(size).keySet());
+	}
+
+	public static void main(String[] args) {
+		Countries.print(capitals(10));
+		Countries.print(names(10));
+		Countries.print(new HashMap<String, String>(capitals(3)));
+		Countries.print(new LinkedHashMap<String, String>(capitals(3)));
+		Countries.print(new TreeMap<String, String>(capitals(3)));
+		Countries.print(new Hashtable<String, String>(capitals(3)));
+		Countries.print(new HashSet<String>(names(6)));
+		Countries.print(new LinkedHashSet<String>(names(6)));
+		Countries.print(new TreeSet<String>(names(6)));
+		Countries.print(new ArrayList<String>(names(6)));
+		Countries.print(new LinkedList<String>(names(6)));
+		Countries.print(capitals().get("BRAZIL"));
+	}
+} /*
+ * Output: {ALGERIA=Algiers, ANGOLA=Luanda, BENIN=Porto-Novo, BOTSWANA=Gaberone,
+ * BULGARIA=Sofia, BURKINA FASO=Ouagadougou, BURUNDI=Bujumbura,
+ * CAMEROON=Yaounde, CAPE VERDE=Praia, CENTRAL AFRICAN REPUBLIC=Bangui}
+ * [ALGERIA, ANGOLA, BENIN, BOTSWANA, BULGARIA, BURKINA FASO, BURUNDI, CAMEROON,
+ * CAPE VERDE, CENTRAL AFRICAN REPUBLIC] {BENIN=Porto-Novo, ANGOLA=Luanda,
+ * ALGERIA=Algiers} {ALGERIA=Algiers, ANGOLA=Luanda, BENIN=Porto-Novo}
+ * {ALGERIA=Algiers, ANGOLA=Luanda, BENIN=Porto-Novo} {ALGERIA=Algiers,
+ * ANGOLA=Luanda, BENIN=Porto-Novo} [BULGARIA, BURKINA FASO, BOTSWANA, BENIN,
+ * ANGOLA, ALGERIA] [ALGERIA, ANGOLA, BENIN, BOTSWANA, BULGARIA, BURKINA FASO]
+ * [ALGERIA, ANGOLA, BENIN, BOTSWANA, BULGARIA, BURKINA FASO] [ALGERIA, ANGOLA,
+ * BENIN, BOTSWANA, BULGARIA, BURKINA FASO] [ALGERIA, ANGOLA, BENIN, BOTSWANA,
+ * BULGARIA, BURKINA FASO] Brasilia
+ */// :~
